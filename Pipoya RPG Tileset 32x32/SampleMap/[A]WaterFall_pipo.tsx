b73783f183205bafd3893aab7a18cbd0a831ff32
tost <tobias.steil@gmx.net>
2021-01-26 12:06:58 +0100

    eigenesOffice

---
+++ Pipoya RPG Tileset 32x32/SampleMap/[A]WaterFall_pipo.tsx
@@ -0,0 +1,156 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="[A]WaterFall_pipo" tilewidth="32" tileheight="32" tilecount="576" columns="32">
+ <image source="[A]WaterFall_pipo.png" width="1024" height="576"/>
+ <tile id="196">
+  <animation>
+   <frame tileid="196" duration="100"/>
+   <frame tileid="204" duration="100"/>
+   <frame tileid="212" duration="100"/>
+   <frame tileid="220" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="197">
+  <animation>
+   <frame tileid="197" duration="100"/>
+   <frame tileid="205" duration="100"/>
+   <frame tileid="213" duration="100"/>
+   <frame tileid="221" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="198">
+  <animation>
+   <frame tileid="198" duration="100"/>
+   <frame tileid="206" duration="100"/>
+   <frame tileid="214" duration="100"/>
+   <frame tileid="222" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="199">
+  <animation>
+   <frame tileid="199" duration="100"/>
+   <frame tileid="207" duration="100"/>
+   <frame tileid="215" duration="100"/>
+   <frame tileid="223" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="212">
+  <animation>
+   <frame tileid="212" duration="100"/>
+   <frame tileid="220" duration="100"/>
+   <frame tileid="196" duration="100"/>
+   <frame tileid="204" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="228">
+  <animation>
+   <frame tileid="228" duration="100"/>
+   <frame tileid="236" duration="100"/>
+   <frame tileid="244" duration="100"/>
+   <frame tileid="252" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="229">
+  <animation>
+   <frame tileid="229" duration="100"/>
+   <frame tileid="237" duration="100"/>
+   <frame tileid="245" duration="100"/>
+   <frame tileid="253" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="230">
+  <animation>
+   <frame tileid="230" duration="100"/>
+   <frame tileid="238" duration="100"/>
+   <frame tileid="246" duration="100"/>
+   <frame tileid="254" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="231">
+  <animation>
+   <frame tileid="231" duration="100"/>
+   <frame tileid="239" duration="100"/>
+   <frame tileid="247" duration="100"/>
+   <frame tileid="255" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="236">
+  <animation>
+   <frame tileid="236" duration="100"/>
+   <frame tileid="244" duration="100"/>
+   <frame tileid="252" duration="100"/>
+   <frame tileid="228" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="244">
+  <animation>
+   <frame tileid="244" duration="100"/>
+   <frame tileid="252" duration="100"/>
+   <frame tileid="228" duration="100"/>
+   <frame tileid="236" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="252">
+  <animation>
+   <frame tileid="252" duration="100"/>
+   <frame tileid="228" duration="100"/>
+   <frame tileid="236" duration="100"/>
+   <frame tileid="244" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="260">
+  <animation>
+   <frame tileid="260" duration="100"/>
+   <frame tileid="268" duration="100"/>
+   <frame tileid="276" duration="100"/>
+   <frame tileid="284" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="261">
+  <animation>
+   <frame tileid="261" duration="100"/>
+   <frame tileid="269" duration="100"/>
+   <frame tileid="277" duration="100"/>
+   <frame tileid="285" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="262">
+  <animation>
+   <frame tileid="262" duration="100"/>
+   <frame tileid="270" duration="100"/>
+   <frame tileid="278" duration="100"/>
+   <frame tileid="286" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="263">
+  <animation>
+   <frame tileid="263" duration="100"/>
+   <frame tileid="271" duration="100"/>
+   <frame tileid="279" duration="100"/>
+   <frame tileid="287" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="268">
+  <animation>
+   <frame tileid="268" duration="100"/>
+   <frame tileid="276" duration="100"/>
+   <frame tileid="284" duration="100"/>
+   <frame tileid="260" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="276">
+  <animation>
+   <frame tileid="276" duration="100"/>
+   <frame tileid="284" duration="100"/>
+   <frame tileid="260" duration="100"/>
+   <frame tileid="268" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="284">
+  <animation>
+   <frame tileid="284" duration="100"/>
+   <frame tileid="260" duration="100"/>
+   <frame tileid="268" duration="100"/>
+   <frame tileid="276" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
